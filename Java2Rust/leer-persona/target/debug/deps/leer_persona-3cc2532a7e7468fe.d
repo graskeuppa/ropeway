f23b7a425139d35b75c4c467c98ec268f294f8c8
graskeuppa <birdlandbollas@ciencias.unam.mx>
2025-05-09 20:35:25 -0600

    Oh

--- Java2Rust/leer-persona/target/debug/deps/leer_persona-3cc2532a7e7468fe.d
+++
@@ -1,5 +0,0 @@
-/home/bollas/ropeway/Rust/leer-persona/target/debug/deps/libleer_persona-3cc2532a7e7468fe.rmeta: src/main.rs
-
-/home/bollas/ropeway/Rust/leer-persona/target/debug/deps/leer_persona-3cc2532a7e7468fe.d: src/main.rs
-
-src/main.rs:
