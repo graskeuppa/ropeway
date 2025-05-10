bec7bf5733639e7f907e4a475353ff01085f3ceb
graskeuppa <birdlandbollas@ciencias.unam.mx>
2025-05-09 20:30:34 -0600

    Serialization tests between Java and Rust

---
+++ Java2Rust/leer-persona/target/debug/build/proc-macro2-a253c229b5c80b86/build_script_build-a253c229b5c80b86.d
@@ -0,0 +1,5 @@
+/home/bollas/ropeway/Rust/leer-persona/target/debug/build/proc-macro2-a253c229b5c80b86/build_script_build-a253c229b5c80b86: /home/bollas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
+
+/home/bollas/ropeway/Rust/leer-persona/target/debug/build/proc-macro2-a253c229b5c80b86/build_script_build-a253c229b5c80b86.d: /home/bollas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
+
+/home/bollas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs:
---
+++ Java2Rust/leer-persona/target/debug/deps/leer_persona-3cc2532a7e7468fe.d
@@ -0,0 +1,5 @@
+/home/bollas/ropeway/Rust/leer-persona/target/debug/deps/libleer_persona-3cc2532a7e7468fe.rmeta: src/main.rs
+
+/home/bollas/ropeway/Rust/leer-persona/target/debug/deps/leer_persona-3cc2532a7e7468fe.d: src/main.rs
+
+src/main.rs:
